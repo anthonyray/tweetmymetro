6bb525c4756a1c293a9e01f8db1ffc66906e7264
anthonyray <dantho361@hotmail.com>
2013-11-16 16:53:42 -0800

    First commit

---
+++ server.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var http = require('http');
+var io = require('socket.io');
+var fs = require('fs');
+var twitter = require('ntwitter');
+
+// Web server
+
+var app = express();
+var server = http.createServer(app);
+
+app.set('port', process.env.PORT || 80);
+app.set('views', __dirname + '/views');
+app.set('view engine','jade');
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+
+// Connection to the Twitter Stream API
+
+var twi = new twitter({
+	consumer_key : "bZXHA5Gz2kqGxFyN9IG5w",
+	consumer_secret:"glijqVU4rCS67WVLdM1bB09IZxwdVOR712adtv3P3fM",
+	access_token_key:"754996698-1YV1XmZgY7fTaDoDojt6pNWKTaK4D3ZY5Mo0Odr8",
+	access_token_secret:"3BAqcYkMgJwHS4ICl0ly6HiNEXHbBo5FiRRmVeOY7w"
+});
+
+// M
+var file = __dirname+'/stations.json';
+var stations = [];
+fs.readFile(file,'utf8',function(err,data){
+	if (err){
+		console.log("Error :"+err);
+	}
+	else
+	{
+		stations = JSON.parse(data);
+		console.log("Server read "+stations.length+" metro stations");
+	}
+})
+
+
+twi.stream('statuses/filter',{track:'ligne5,ligne6,ligne4,ligne1,ligne2,chatelet,invalides,odeon,saint michel,montparnasse,raspail,parmentier,gnd'},function(stream){
+	
+	console.log("Now listening to twitter ...");
+	stream.on('data',function(data){
+		
+		console.log(data.user.screen_name + " : " + data.text);
+	});
+
+	stream.on('end', function(response){
+		// Handle a disconnection
+	});
+
+	stream.on('destroy',function(response){
+		// Handle a silent disconnection from twitter
+	});
+
+	
+});
